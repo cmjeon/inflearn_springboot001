73e3312889fbca0150dc3314bf8cd3898ad0d28a
cmj <chmin82@gmail.com>
2023-02-10 22:46:35 +0900

    [CHORE] 의존 오브젝트 DI 적용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -18,6 +18,7 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		GenericApplicationContext applicationContext = new GenericApplicationContext();
 		applicationContext.registerBean(HelloController.class);
+		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
--- src/main/java/tobyspring/helloboot/SimpleHelloService.java
+++ src/main/java/tobyspring/helloboot/SimpleHelloService.java
@@ -1,8 +1,9 @@
 package tobyspring.helloboot;
 
-public class SimpleHelloService {
+public class SimpleHelloService implements HelloService {
 
-    String sayHello(String name) {
+    @Override
+    public String sayHello(String name) {
         return "Hello " + name;
     }
 }

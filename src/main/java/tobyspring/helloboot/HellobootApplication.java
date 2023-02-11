b7949fa50cd48ecc3bef6bf72128b069ecf56e4c
cmj <chmin82@gmail.com>
2023-02-11 16:54:24 +0900

    [CHORE] 자바코드 구성 정보 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,14 +2,28 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class HellobootApplication {
+
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
 		// 스프링 컨테이너를 생성, 빈을 등록하고 초기화
-		GenericWebApplicationContext webApplicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -23,9 +37,8 @@ public class HellobootApplication {
 				webServer.start();
 			}
 		};
-		webApplicationContext.registerBean(HelloController.class);
-		webApplicationContext.registerBean(SimpleHelloService.class);
-		webApplicationContext.refresh();
+		applicationContext.register(HellobootApplication.class);
+		applicationContext.refresh();
 	}
 
 }

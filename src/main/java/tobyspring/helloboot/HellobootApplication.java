d89bbecd96a8af24ae7172b68e9cac737d4c1d75
cmj <chmin82@gmail.com>
2023-02-11 16:45:14 +0900

    [CHORE] 스프링 컨테이너로 통합

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -8,18 +8,24 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext webApplicationContext = new GenericWebApplicationContext();
+		// 스프링 컨테이너를 생성, 빈을 등록하고 초기화
+		GenericWebApplicationContext webApplicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+				// 스프링 컨테이너를 활용해서 서블릿 컨테이너를 만들고, 필요한 디스패처 서블릿을 등록하는 서블릿 컨테이너 초기화
+				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+						new DispatcherServlet(this)
+					).addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
 		webApplicationContext.registerBean(HelloController.class);
 		webApplicationContext.registerBean(SimpleHelloService.class);
 		webApplicationContext.refresh();
-
-		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-				new DispatcherServlet(webApplicationContext)
-			).addMapping("/*");
-		});
-		webServer.start();
 	}
 
 }

038aea66b02dd9c8de5e0a85a8455d361d400655
cmj <chmin82@gmail.com>
2023-02-20 21:46:11 +0900

    [CHORE] @Value와 PropertySourcesPlaceholderConfigurer

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,10 +1,10 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
@@ -12,11 +12,14 @@ import tobyspring.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 

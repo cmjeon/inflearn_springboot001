38d53bc0b8bc1caed3e9f702d33b9b880de4b3b4
cmj <chmin82@gmail.com>
2023-02-20 22:05:05 +0900

    [CHORE] 프로퍼티 클래스의 분리

--- src/test/java/tobyspring/helloboot/HelloApitest.java
+++ src/test/java/tobyspring/helloboot/HelloApitest.java
@@ -17,7 +17,7 @@ public class HelloApitest {
         TestRestTemplate rest = new TestRestTemplate();
 
         // when
-        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/app/hello?name={name}", String.class, "Spring");
+        ResponseEntity<String> res = rest.getForEntity("http://localhost:9090/app/hello?name={name}", String.class, "Spring");
 
         // then
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -31,7 +31,7 @@ public class HelloApitest {
         TestRestTemplate rest = new TestRestTemplate();
 
         // when
-        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/app/hello?name=", String.class);
+        ResponseEntity<String> res = rest.getForEntity("http://localhost:9090/app/hello?name=", String.class);
 
         // then
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
